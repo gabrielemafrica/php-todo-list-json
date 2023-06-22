2a725597a1a2cc5ebc81ff71a88101b8945a7990
gabrielemafrica <gabriele.ga@live.it>
2023-06-22 17:16:01 +0300

    connessione con backeand

---
+++ back-end/back-and.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+$string = file_get_contents('data.json');
+$todo = json_decode($string, true);
+
+echo "<pre>";
+var_dump($todo);
+echo "</pre>";
---
+++ front-end/src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      todoList: [],
+    
+      newTodo: {
+        to: "",
+        do: false
+      }
+    };
+  },
+  methods: {
+    
+  },
+  mounted() {
+    axios.get('http://localhost:8888/giorno58/php-todo-list-json/back-end/back-and.php')
+    .then(response => {
+        this.todoList = response.data;
+      });
+  },
+
+
+
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>todo</h1>
+    <ul>
+      <li v-for="(todo, index) in todoList" :key="index">
+
+        {{todoList.to}}
+      
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<style>
+
+</style>
